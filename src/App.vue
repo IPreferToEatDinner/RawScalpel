167adc81f5bd5e2bc434ff86a753d97b5bba98cf
ZSR <2472990740@qq.com>
2023-07-12 01:04:04 +0800

    引入scss，并开始设计界面

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,44 @@
 <script setup lang="ts">
+import Setting from '../src/components/settings/Settings.vue'
 </script>
 
-<template></template>
+<template>
+  <el-container>
+    <el-header>
+      Header content
+      <div class="right">
+        <Setting></Setting>
+      </div>
+    </el-header>
+    <el-container>
+      <el-aside>
+        Aside content
+      </el-aside>
 
-<style scoped></style>
+      <el-main>
+        Main content
+      </el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<style scoped lang="scss">
+.el-container {
+  height: 100%;
+
+  .el-header {
+    display: flex;
+
+
+    height: 35px;
+    background-color: rgb(106, 79, 151);
+
+    .right {}
+  }
+
+  .el-aside {
+    width: 75%;
+    background-color: lightblue;
+  }
+}
+</style>
---
+++ src/components/settings/Settings.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <img src="" alt="">
+  </div>
+</template>
+
+<style scoped lang="scss">
+.div {}
+</style>
+
+<script setup lang="ts">
+</script>
