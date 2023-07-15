8ce0ff5c022786c0c438a42784fc33a0993f3956
ZSR <2472990740@qq.com>
2023-07-15 13:52:50 +0800

    estalish the toolbar base

--- src/components/toolBar/ToolBar.vue
+++ src/components/toolBar/ToolBar.vue
@@ -1,17 +1,28 @@
 <template>
   <div class="background">
-    <div class="up" ref="up" @click.capture="activeMode">
+    <div class="contain" ref="up" @click="activeMode($event, up)">
 
       <div class="item">
         <img :src="editMode" alt="editMode">
       </div>
 
       <div class="item">
-        <img :src="editMode" alt="presets">
+        <img :src="presetMode" alt="presetMode">
       </div>
 
     </div>
-    <div></div>
+
+
+    <div class="contain" ref="down" @click="activeMode($event, down)">
+
+      <div class="item">
+        <img :src="drag" alt="drag">
+      </div>
+
+      <div class="item">
+        <img :src="zoom" alt="drag">
+      </div>
+    </div>
   </div>
 </template>
 
@@ -20,11 +31,12 @@
   display: flex;
   flex-direction: column;
   align-items: center;
+  justify-content: space-between;
   background-color: #4F3073;
   width: 45px;
   padding-left: 3px;
 
-  .up {
+  .contain {
     .item {
       display: flex;
       align-items: center;
@@ -32,9 +44,9 @@
 
       height: 30px;
       width: 30px;
-      margin: 10px;
+      margin: 10px 0;
 
-      transition: all 0.4s;
+      transition: all 0.2s;
 
       img {
         height: 75%;
@@ -58,20 +70,33 @@
 
 <script setup lang="ts">
 import editMode from '@/assets/images/editMode.svg'
-import { ref } from 'vue';
+import presetMode from '@/assets/images/presetMode.svg';
+import drag from '@/assets/images/drag.svg';
+import zoom from '@/assets/images/zoom.svg'
+import { Ref, ref } from 'vue';
 
-const up = ref(null)
+const up: Ref = ref(null)
+const down: Ref = ref(null)
 
-enum Mode { 'editMode', 'presets' };
+enum Mode { 'editMode', 'presetMode' };
 
-function activeMode(event: Event) {
-  // set active class on this button
-  let getNode = event.target as HTMLElement;
-  if ((getNode?.parentNode as HTMLElement)?.classList.contains('item')) {
-    (up.value as unknown as HTMLDivElement).childNodes.forEach((node) => {
+function activeMode(event: Event, container: Ref) {
+  // clear active
+  const clearActive = (container: Ref) => {
+    (container as unknown as HTMLDivElement).childNodes.forEach((node) => {
       (node as HTMLElement).classList.remove('active')
     })
-    getNode?.parentElement?.classList.add('active')
+  }
+
+  // set active class on this button
+  let getNode = event.target as HTMLElement;
+
+  if ((getNode?.parentNode as HTMLElement)?.classList.contains('item')) {
+    clearActive(container)
+    getNode?.parentElement?.classList.toggle('active')
+  } else if (getNode.classList.contains('item')) {
+    clearActive(container)
+    getNode.classList.toggle('active')
   }
 }
 
